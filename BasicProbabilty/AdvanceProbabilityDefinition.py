2d8c814b81ef00b969f08933a4a38c4e6271fa65
Rakesh Mummana <mummanarakesh@gmail.com>
2025-07-18 10:33:43 +0530

    add a advance probability definition

---
+++ BasicProbabilty/AdvanceProbabilityDefinition.py
@@ -0,0 +1,21 @@
+import random
+import matplotlib.pyplot as plt
+coin = ["T", "H"]
+
+def flip_coin():
+    return random.choice(coin)
+
+heads = 0
+series = []
+for i in range(1, 100):
+    result = flip_coin()
+    if result == "H":
+        heads += 1
+    series.append(heads/i)
+print(f"series: {series}")
+
+plt.plot(series)
+plt.xlabel("Number of flips")
+plt.ylabel("Proportion of Heads")
+plt.title("Coin Flip Simulation")
+plt.show()
---
+++ BasicProbabilty/Limit.py
@@ -0,0 +1,8 @@
+# for i in range(1,10000000):
+#     result = 2*i / (i + 1)
+
+# print(f'Limit of 2n/(2n+1) as n approaches infinity: {result:.10f}')
+
+for i in range(1, 1000000):
+    result = (i + 1) / i
+print(f'Limit of (n+1)/n as n approaches infinity: {result:.10f}')
